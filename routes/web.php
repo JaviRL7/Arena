22b549adda3ee248c71894f1fdc4a3e77df9a203
JaviRL7 <Jrlsanlucar11@gmail.com>
2023-11-18 01:32:02 +0100

    Remake

--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,7 @@
 <?php
 
-use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\AdminTeamsController;
-use App\Http\Controllers\PlayersController;
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -18,29 +16,3 @@ use App\Http\Controllers\PlayersController;
 Route::get('/', function () {
     return view('welcome');
 });
-
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
-
-Route::middleware('auth')->group(function () {
-    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-});
-Route::get('/teams', [AdminTeamsController::class, 'index']);
-
-require __DIR__.'/auth.php';
-
-
-
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
-
-
-Route::get('/players', [PlayersController::class, 'index'])->name('players.index');
-Route::get('/players/create', [PlayersController::class, 'create']);
-Route::post('/players', [PlayersController::class, 'store']);
-Route::get('/players/{player}', [PlayersController::class, 'show'])->name('players.show');
-Route::get('/players/{player}/edit', [PlayersController::class, 'edit'])->name('players.edit');
-Route::put('/players/{player}', [PlayersController::class, 'update'])->name('players.update');
-Route::delete('/players/{id}', [PlayersController::class, 'destroy'])->name('players.destroy');
