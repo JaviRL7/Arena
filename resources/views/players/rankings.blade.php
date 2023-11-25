babee96ca70052cf8e3ef61a7f44f40eef2258ed
JaviRL7 <Jrlsanlucar11@gmail.com>
2023-11-25 03:42:23 +0100

    Ranking retoque

--- resources/views/players/rankings.blade.php
+++ resources/views/players/rankings.blade.php
@@ -2,51 +2,15 @@
 @section('title', 'rankings players')
 
 @section('content')
-<div class="flex flex-col">
-    <div class="-my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
-      <div class="py-2 align-middle inline-block min-w-full sm:px-6 lg:px-8">
-        <div class="shadow overflow-hidden border-b border-gray-200 sm:rounded-lg">
-          <table class="min-w-full divide-y divide-gray-200">
-            <thead class="bg-gray-50">
-              <tr>
-                <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
-                  Nombre
-                </th>
-                <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
-                  Kills
-                </th>
-              </tr>
-            </thead>
-            <tbody class="bg-white divide-y divide-gray-200">
-              @foreach ($players as $player)
-                <tr>
-                  <td class="px-6 py-4 whitespace-nowrap">
-                    <div class="flex items-center">
-                      <div class="flex-shrink-0 h-12 w-12">
-                        <img class="h-12 w-12 rounded-full" src="{{ $player->photo }}" alt="">
-                      </div>
-                      <div class="ml-4">
-                        <div class="text-sm font-medium text-gray-900">
-                          {{ $player->nick }}
-                        </div>
-                      </div>
-                    </div>
-                  </td>
-                  <td class="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
-                    {{ $player->total_kills }}
-                  </td>
-                  <td>
-                  <div class="flex-shrink-0 h-10 w-auto align-center">
-                    <img class="h-10 w-auto" src="{{ $player->teams->first()->logo }}" alt="">
-                  </div>
-                </td>
-                </tr>
-              @endforeach
-            </tbody>
-          </table>
-        </div>
-      </div>
-    </div>
-  </div>
-
+    <div class="grid grid-cols-3 gap-4">
+        @foreach ($players as $player)
+          <div class="flex items-center space-x-4">
+              <img class="h-12 w-12 rounded-full" src="{{ $player->photo }}" alt="">
+                <div class="flex-shrink-0 h-10 w-auto align-center">
+                  <img class="h-10 w-auto" src="{{ $player->teams->first()->logo }}" alt="">
+                </div>
+              <div class="text-lg font-medium text-gray-900">{{ $player->nick }}</div>
+            <div class="text-lg font-medium text-gray-900">{{ $player->total_kills }}</div>
+          </div>
+        @endforeach
 @endsection
