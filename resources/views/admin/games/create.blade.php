ee7580bcc58564364ddcf9d0da77bea3052e1a9c
JaviRL7 <Jrlsanlucar11@gmail.com>
2023-12-06 05:34:15 +0100

    create teams sin date ni enviar

--- resources/views/admin/games/create.blade.php
+++ resources/views/admin/games/create.blade.php
@@ -1,104 +1,91 @@
-<!DOCTYPE html>
-<html>
-
-<head>
-    <title>Tu Título Aquí</title>
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-    <script>
-        $(document).ready(function() {
-            $("#teamId").submit(function(e) {
-                e.preventDefault();
-                // Borra los formularios existentes
-                $("form[id^='playerForm']").remove();
-                var team1Id = $("#teamBlueid").val();
-                var team2Id = $("#teamRedid").val();
-                $.ajax({
-                    url: '/admin/games/create/' + team1Id + '/' + team2Id,
-                    type: 'GET',
-                    success: function(data) {
-                        data.team1Players.forEach(function(player) {
-                            var form = $('<form>').attr('id', 'playerForm' + player.id);
-                            var labelKills = $('<label>').text('Introduce kills para ' +
-                                player.nick);
-                            var inputKills = $('<input>').attr('type', 'number').attr(
-                                'name', 'kills');
-                            var labelDeaths = $('<label>').text(
-                                'Introduce deaths para ' + player.nick);
-                            var inputDeaths = $('<input>').attr('type', 'number').attr(
-                                'name', 'deaths');
-                            var labelAssists = $('<label>').text(
-                                'Introduce assists para ' + player.nick);
-                            var inputAssists = $('<input>').attr('type', 'number').attr(
-                                'name', 'assists');
-                            var labelChampion = $('<label>').text(
-                                'Selecciona el campeón para ' + player.nick);
-                            var selectChampion = $('<select>').attr('name',
-                                'championId');
-                            data.champions.forEach(function(champion) {
-                                var option = $('<option>').attr('value',
-                                    champion.id).text(champion.name);
-                                selectChampion.append(option);
-                            });
-                            form.append(labelKills, inputKills, labelDeaths,
-                                inputDeaths, labelAssists, inputAssists,
-                                labelChampion, selectChampion);
-                            $('body').append(form);
-                        });
-                        data.team2Players.forEach(function(player) {
-                            var form = $('<form>').attr('id', 'playerForm' + player.id);
-                            var labelKills = $('<label>').text('Introduce kills para ' +
-                                player.nick);
-                            var inputKills = $('<input>').attr('type', 'number').attr(
-                                'name', 'kills');
-                            var labelDeaths = $('<label>').text(
-                                'Introduce deaths para ' + player.nick);
-                            var inputDeaths = $('<input>').attr('type', 'number').attr(
-                                'name', 'deaths');
-                            var labelAssists = $('<label>').text(
-                                'Introduce assists para ' + player.nick);
-                            var inputAssists = $('<input>').attr('type', 'number').attr(
-                                'name', 'assists');
-                            var labelChampion = $('<label>').text(
-                                'Selecciona el campeón para ' + player.nick);
-                            var selectChampion = $('<select>').attr('name',
-                                'championId');
-                                data.champions.forEach(function(champion) {
-                                var option = $('<option>').attr('value',
-                                    champion.id).text(champion.name);
-                                selectChampion.append(option);
-                            });
-                            // Añade las opciones al select de campeones aquí
-                            form.append(labelKills, inputKills, labelDeaths,
-                                inputDeaths, labelAssists, inputAssists,
-                                labelChampion, selectChampion);
-                            $('body').append(form);
-                        });
-                    }
-                });
+@extends('layouts.plantilla_admin')
+@section('title', 'Teams index')
+@section('script')
+  <script>
+    $(document).ready(function () {
+      $("#teamId").submit(function (e) {
+        e.preventDefault();
+        // Borra los formularios existentes
+        $("form[id^='playerForm']").remove();
+        var team1Id = $("#teamBlueid").val();
+        var team2Id = $("#teamRedid").val();
+        var matchDate = $("#matchDate").val();
+        $.ajax({
+          url: '/admin/games/create/' + team1Id + '/' + team2Id + '?date=' + matchDate,
+          type: 'GET',
+          success: function (data) {
+            data.team1Players.forEach(function (player) {
+              var form = $('<form>').attr('id', 'playerForm' + player.id);
+              var labelSelectData = $('<label>').text('Selecciona los datos del jugador (' + player.nick + '):');
+              var labelKills = $('<label>').text('Kills:');
+              var inputKills = $('<input>').attr('type', 'number').attr('name', 'kills').addClass('form-control col-md-6 mb-3');
+              var labelDeaths = $('<label>').text('Deaths:');
+              var inputDeaths = $('<input>').attr('type', 'number').attr('name', 'deaths').addClass('form-control col-md-6 mb-3');
+              var labelAssists = $('<label>').text('Assists:');
+              var inputAssists = $('<input>').attr('type', 'number').attr('name', 'assists').addClass('form-control col-md-6 mb-3');
+              var labelChampion = $('<label>').text('Selecciona el campeón:');
+              var selectChampion = $('<select>').attr('name', 'championId').addClass('form-control col-md-6 mb-3');
+              data.champions.forEach(function (champion) {
+                var option = $('<option>').attr('value', champion.id).text(champion.name);
+                selectChampion.append(option);
+              });
+              form.append(labelSelectData, labelKills, inputKills, labelDeaths, inputDeaths, labelAssists, inputAssists, labelChampion, selectChampion);
+              $('#teamBlue').append(form);
             });
+            data.team2Players.forEach(function (player) {
+              var form = $('<form>').attr('id', 'playerForm' + player.id);
+              var labelSelectData = $('<label>').text('Selecciona los datos del jugador (' + player.nick + '):');
+              var labelKills = $('<label>').text('Kills:');
+              var inputKills = $('<input>').attr('type', 'number').attr('name', 'kills').addClass('form-control col-md-6 mb-3');
+              var labelDeaths = $('<label>').text('Deaths:');
+              var inputDeaths = $('<input>').attr('type', 'number').attr('name', 'deaths').addClass('form-control col-md-6 mb-3');
+              var labelAssists = $('<label>').text('Assists:');
+              var inputAssists = $('<input>').attr('type', 'number').attr('name', 'assists').addClass('form-control col-md-6 mb-3');
+              var labelChampion = $('<label>').text('Selecciona el campeón:');
+              var selectChampion = $('<select>').attr('name', 'championId').addClass('form-control col-md-6 mb-3');
+              data.champions.forEach(function (champion) {
+                var option = $('<option>').attr('value', champion.id).text(champion.name);
+                selectChampion.append(option);
+              });
+              form.append(labelSelectData, labelKills, inputKills, labelDeaths, inputDeaths, labelAssists, inputAssists, labelChampion, selectChampion);
+              $('#teamRed').append(form);
+            });
+          }
         });
-    </script>
-</head>
-
-<body>
+      });
+    });
+  </script>
+  @endsection
+@section('select_teams')
     <h1>Formulario teams</h1>
     <form id="teamId">
         <label for="teamBlueid">Elige el equipo azul:</label><br>
-        <select id="teamBlueid" name="teamBlueid">
+        <select id="teamBlueid" name="teamBlueid" class="form-control mb-3">
             <option value="" selected>Elige el equipo azul</option>
             @foreach ($teams as $team)
                 <option value="{{ $team->id }}">{{ $team->name }}</option>
             @endforeach
         </select><br>
         <label for="teamRedid">Elige el equipo rojo:</label><br>
-        <select id="teamRedid" name="teamRedid">
+        <select id="teamRedid" name="teamRedid" class="form-control mb-3">
             <option value="" selected>Elige el equipo rojo</option>
             @foreach ($teams as $team)
                 <option value="{{ $team->id }}">{{ $team->name }}</option>
             @endforeach
         </select><br>
-        <input type="submit" value="Enviar">
+        <input type="submit" value="Enviar" class="btn btn-primary">
     </form>
-</body>
+@endsection
+@section('team_blue')
+    <!-- Team Blue Form Section -->
+    <div id="teamBlue">
+        <!-- The forms for Team Blue will be appended here -->
+    </div>
+@endsection
 
-</html>
+@section('team_red')
+    <!-- Team Red Form Section -->
+    <div id="teamRed">
+        <!-- The forms for Team Red will be appended here -->
+    </div>
+@endsection
---
+++ resources/views/layouts/plantilla_admin.blade.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>@yield('title')</title>
+    @vite('resources/css/app.css')
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+</head>
+<body>
+
+    {{-- Navbar --}}
+    @include('layouts.navbar')
+    <main>
+        @include('layouts.grid_admin')
+    </main>
+
+    <!-- Scripts at the bottom -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
+    @yield('script')
+
+</body>
+</html>
