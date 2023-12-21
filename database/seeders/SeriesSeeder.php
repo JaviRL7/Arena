77105915f70c7f1a7e9a2d6eb95632723b015682
JaviRL7 <Jrlsanlucar11@gmail.com>
2023-12-21 07:00:02 +0100

    Carrusel casi bien pero se mueve a la derecha el finale

--- database/seeders/SeriesSeeder.php
+++ database/seeders/SeriesSeeder.php
@@ -13,7 +13,7 @@ class SeriesSeeder extends Seeder
      */
     public function run(): void
     {
-        Serie::create(['team_1_id' => 1, 'team_2_id' => 2, 'competition_id' => 1, 'name' => 'Finale ', 'type' => 'bo5', 'date' => Carbon::createFromFormat('d/m/Y', '19/11/2023'), 'competition_id' => 2 ]);
+        Serie::create(['team_1_id' => 1, 'team_2_id' => 2, 'competition_id' => 1, 'name' => 'Finale', 'type' => 'bo5', 'date' => Carbon::createFromFormat('d/m/Y', '19/11/2023'), 'competition_id' => 2 ]);
         //serie t1-jdg
         Serie::create(['team_1_id' => 2, 'team_2_id' => 4, 'competition_id' => 1, 'name' => 'Semi-final', 'type' => 'bo5', 'date' => Carbon::createFromFormat('d/m/Y', '19/11/2023'), 'competition_id' => 2 ]);
 
