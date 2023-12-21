7fe7b415bffc5ff5e6dfda26ee33589e95fa1b28
JaviRL7 <Jrlsanlucar11@gmail.com>
2023-12-21 04:52:38 +0100

    Carrusel de cuartos de final y semis bien de tamaño

--- resources/views/series/index.blade.php
+++ resources/views/series/index.blade.php
@@ -7,12 +7,60 @@
     <div class="container-fluid">
         <div class="row">
             <div class="col-md-12">
-                <div id="carouselExampleIndicators" class="carousel slide my-carousel">
+                @foreach ($series as $serie)
+                    @if ($serie->name == 'Finale')
+                        <img src="{{asset($serie->team_1->team_photo)}}" alt="{{$serie->team_1->name}}"><img src="{{asset($serie->team_2->team_photo)}}" alt="{{$serie->team_2->name}}">
+                    @endif
+                @endforeach
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-md-12">
+                <div id="carouselSemi-finals" class="carousel slide my-carousel">
+                    <div class="carousel-indicators">
+                        @php $count = 0; @endphp
+                        @foreach ($series as $serie)
+                            @if ($serie->name == 'Semi-final')
+                                <button type="button" data-bs-target="#carouselSemi-finals" data-bs-slide-to="{{ $count }}" @if($count == 0) class="active" aria-current="true" @endif aria-label="Slide {{ $count + 1 }}"></button>
+                                @php $count++; @endphp
+                            @endif
+                        @endforeach
+                    </div>
+                    <div class="carousel-inner">
+                        @php $count = 0; @endphp
+                        @foreach ($series as $serie)
+                            @if ($serie->name == 'Semi-final')
+                                <div class="carousel-item @if($count == 0) active @endif">
+                                    <div class="d-flex">
+                                        <img src="{{ asset($serie->team_1->team_photo) }}" class="d-block w-50" alt="{{ $serie->team_1->name }}">
+                                        <img src="{{ asset($serie->team_2->team_photo) }}" class="d-block w-50" alt="{{ $serie->team_2->name }}">
+                                    </div>
+                                </div>
+                                @php $count++; @endphp
+                            @endif
+                        @endforeach
+                    </div>
+                    <button class="carousel-control-prev" type="button" data-bs-target="#carouselSemi-finals" data-bs-slide="prev">
+                        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                        <span class="visually-hidden">Anterior</span>
+                    </button>
+                    <button class="carousel-control-next" type="button" data-bs-target="#carouselSemi-finals" data-bs-slide="next">
+                        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                        <span class="visually-hidden">Siguiente</span>
+                    </button>
+                </div>
+            </div>
+        </div>
+        <br>
+        <br>
+        <div class="row">
+            <div class="col-md-12">
+                <div id="carouselQuarterFinals" class="carousel slide my-carousel">
                     <div class="carousel-indicators">
                         @php $count = 0; @endphp
                         @foreach ($series as $serie)
                             @if ($serie->name == 'Quarterfinals')
-                                <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="{{ $count }}" @if($count == 0) class="active" aria-current="true" @endif aria-label="Slide {{ $count + 1 }}"></button>
+                                <button type="button" data-bs-target="#carouselQuarterFinals" data-bs-slide-to="{{ $count }}" @if($count == 0) class="active" aria-current="true" @endif aria-label="Slide {{ $count + 1 }}"></button>
                                 @php $count++; @endphp
                             @endif
                         @endforeach
@@ -31,11 +79,11 @@
                             @endif
                         @endforeach
                     </div>
-                    <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
+                    <button class="carousel-control-prev" type="button" data-bs-target="#carouselQuarterFinals" data-bs-slide="prev">
                         <span class="carousel-control-prev-icon" aria-hidden="true"></span>
                         <span class="visually-hidden">Anterior</span>
                     </button>
-                    <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
+                    <button class="carousel-control-next" type="button" data-bs-target="#carouselQuarterFinals" data-bs-slide="next">
                         <span class="carousel-control-next-icon" aria-hidden="true"></span>
                         <span class="visually-hidden">Siguiente</span>
                     </button>
@@ -43,4 +91,5 @@
             </div>
         </div>
     </div>
+    <br>
 @endsection
