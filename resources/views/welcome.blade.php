ea649a4ac1a6ff86d906ad4b2c6e1a0e3885c9bc
Javier Rodriguez <Jrlsanlucar11@gmail.com>
2023-12-22 10:03:01 +0100

    Vista de welcome mejorada

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,4 +1,3 @@
-
 @extends('layouts.plantilla')
 @section('title', 'show games')
 @section('css')
@@ -8,24 +7,105 @@
 @section('content')
 
 <div class="container">
-    <div class="row">
-      <div class="col-lg-6" style="height: 600px;">
-        <img src="material/final.jpg" class="img-fluid h-100 w-100 object-fit-cover" alt="Imagen 1">
+    <div class="title-line">
+        <h2>Worlds 23</h2>
       </div>
-      <div class="col-lg-6">
+    <div class="row">
+      <div class="col-lg-6 col-12" style="height: 600px;">
+        <div class="image-container">
+          <a href="ruta-de-la-imagen-1">
+            <img src="material/final.jpg" class="img-fluid h-100 w-100 object-fit-cover" alt="Imagen 1">
+          </a>
+          <div class="image-title">
+            <h1>Check out the World Championship final stats</h1>
+          </div>
+        </div>
+      </div>
+      <div class="col-lg-6 col-12">
         <div class="row h-50">
-          <div class="col-lg-12" style="height: 300px;">
-            <img src="material/hwei.jpg" class="img-fluid h-100 w-100 object-fit-cover" alt="Imagen 2">
+          <div class="col-lg-12 col-12" style="height: 300px;">
+            <div class="image-container">
+              <a href="ruta-de-la-imagen-2">
+                <img src="material/hwei.jpg" class="img-fluid h-100 w-100 object-fit-cover" alt="Imagen 2">
+              </a>
+              <div class="image-title">
+                <h1>Champions with the best win rate</h1>
+              </div>
+            </div>
           </div>
         </div>
         <div class="row h-50">
-          <div class="col-lg-12" style="height: 300px;">
-            <img src="material/gumayusi.jpeg" class="img-fluid h-100 w-100 object-fit-cover" alt="Imagen 3">
+          <div class="col-lg-12 col-12" style="height: 300px;">
+            <div class="image-container">
+              <a href="ruta-de-la-imagen-3">
+                <img src="material/gumayusi.jpeg" class="img-fluid h-100 w-100 object-fit-cover" alt="Imagen 3">
+              </a>
+              <div class="image-title">
+                <h1> Who is the player with the best KDA?</h1>
+              </div>
+            </div>
           </div>
         </div>
       </div>
     </div>
+    <br>
+    <div class="title-line">
+        <h2>Who's the player ?</h2>
+      </div>
+    <div class="row">
+        <div class="col-12" style="height: 300px;">
+            <div class="image-container">
+                <a href="ruta-de-la-imagen-3">
+                  <img src="material/faker.jpg" class="img-fluid h-100 w-100 object-fit-cover" alt="Imagen 3">
+                </a>
+                <div class="image-title">
+                  <h1> Who is the player with the best KDA?</h1>
+                </div>
+              </div>
+        </div>
+    </div>
+    <br>
+    <div class="title-line">
+        <h2>Profile</h2>
+      </div>
+    <div class="row">
+      <div class="col-lg-6 col-12" style="height: 600px;">
+        <div class="image-container">
+          <a href="ruta-de-la-imagen-1">
+            <img src="material/final.jpg" class="img-fluid h-100 w-100 object-fit-cover" alt="Imagen 1">
+          </a>
+          <div class="image-title">
+            <h1>Check out the World Championship final stats</h1>
+          </div>
+        </div>
+      </div>
+      <div class="col-lg-6 col-12">
+        <div class="row h-50">
+          <div class="col-lg-12 col-12" style="height: 300px;">
+            <div class="image-container">
+              <a href="ruta-de-la-imagen-2">
+                <img src="material/hwei.jpg" class="img-fluid h-100 w-100 object-fit-cover" alt="Imagen 2">
+              </a>
+              <div class="image-title">
+                <h1>Champions with the best win rate</h1>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="row h-50">
+          <div class="col-lg-12 col-12" style="height: 300px;">
+            <div class="image-container">
+              <a href="ruta-de-la-imagen-3">
+                <img src="material/gumayusi.jpeg" class="img-fluid h-100 w-100 object-fit-cover" alt="Imagen 3">
+              </a>
+              <div class="image-title">
+                <h1> Who is the player with the best KDA?</h1>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <br>
   </div>
-
 @endsection
-
