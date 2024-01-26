7293101feb5148016a0d03c1f2083f634547a927
JaviRL7 <Jrlsanlucar11@gmail.com>
2024-01-26 09:47:33 +0100

    nuevo estilo

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,21 +1,30 @@
 <div class="footer">
-    <div class="container">
+    <div class="container" style="background-color: #e44445; padding: 20px;">
         <div class="row">
             <div class="col-md-4">
-                <h1 class="footer-title">Gunlim ©</h1>
+                <h1 class="footer-title" style="font-family: mol;">Gunlim ©</h1>
             </div>
             <div class="col-md-4 links">
                 <ul>
-                    <li>Patreon</li>
-                    <li>Twitter</li>
-                    <li>YouTube</li>
-                    <li>Discord</li>
+                    <li><span class="social-icon"><i class="fab fa-patreon"></i></span> <a href="#" class="social-link">Patreon</a></li>
+                    <li><span class="social-icon"><i class="fab fa-twitter"></i></span> <a href="#" class="social-link">Twitter</a></li>
+                    <li><span class="social-icon"><i class="fab fa-youtube"></i></span> <a href="#" class="social-link">YouTube</a></li>
+                    <li><span class="social-icon"><i class="fab fa-discord"></i></span> <a href="#" class="social-link">Discord</a></li>
                 </ul>
             </div>
-            <div class="col-md-4 legal-links">
+            <div class="col-md-4 legal-links" style="font-family: mol;">
                 Aviso Legal<br/>
                 Política de cookies<br/>
             </div>
         </div>
     </div>
+</div>
+<div class="logo-section" style="background-color: #1a1a1a; padding: 25px; text-align: center; display: flex; align-items: center; justify-content: center;">
+    <h5 style="color: white; font-family:mol; margin-right: 30px;">In collaboration with:</h5>
+    <img src="/competition_logo/riot.png" alt="Logo Riot" style="height: 50px; margin-right: 40px;">
+    <img src="/icons/logofinal.png" alt="Logo" class="logo-img">
+    <img src="/competition_logo/lck_w.png" alt="Logo" class="logo-img">
+    <img src="/competition_logo/lcs_w.png" alt="Logo" class="logo-img">
+    <img src="/competition_logo/lec_w.png" alt="Logo" class="logo-img">
+    <img src="/competition_logo/lpl_w.png" alt="Logo" class="logo-img">
 </div>
--- resources/views/layouts/navbar.blade.php
+++ resources/views/layouts/navbar.blade.php
@@ -1,86 +1,89 @@
-<!--Navbar-->
-<nav class="navbar navbar-expand-lg fixed-top" style="background-color: #fffcdc; padding: 0 15px;">
+<nav class="navbar navbar-expand-lg fixed-top" style="background-color: #2c2c26; padding: 0 15px;">
     <div class="container-fluid">
         <!--logo-->
-        <a class="navbar-brand fs-4 d-flex align-items-center me-5" href="#">
-            <img src="/icons/logof.png" alt="Logo" style="height: 50px;">
-            <h2 style="font-family: important; color: #e44445">Gunlim</h2>
+        <a class="navbar-brand fs-4 d-flex align-items-center me-5" href="#"
+            style="margin-top: 10px; margin-left: 90px;">
+            <img src="/icons/logofinal.png" alt="Logo" style="height: 50px;">
+            <h2 style="font-family: mol; color: whitesmoke; margin-left: 20px; margin-right: 50px;">Gunlim</h2>
         </a>
 
         <!--Button-->
         <button class="navbar-toggler shadow-none border-0" type="button" data-bs-toggle="offcanvas"
-        data-bs-target="#offcanvasNavbar" aria-controls="offcanvasNavbar" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
+            data-bs-target="#offcanvasNavbar" aria-controls="offcanvasNavbar" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
         </button>
 
         <!--side bar-->
         <div class="sidebar offcanvas offcanvas-start" tabindex="-1" id="offcanvasNavbar"
-        aria-labelledby="offcanvasNavbarLabel">
+            aria-labelledby="offcanvasNavbarLabel">
             <!--side bar header-->
             <div class="offcanvas-header border-bottom">
                 <h1 class="offcanvas-title" id="offcanvasNavbarLabel">Gunlin</h1>
                 <button type="button" class="btn-close shadow-none" data-bs-dismiss="offcanvas"
-                aria-label="Close"></button>
+                    aria-label="Close"></button>
             </div>
 
             <!--side bar body-->
             <div class="offcanvas-body d-flex flex-column flex-lg-row p-4 p-lg-0">
                 <ul class="navbar-nav justify-content-lg-start fs-5 flex-grow-1 pe-3">
-                    <li class="nav-item">
-                        <a class="nav-link active" aria-current="page" href="{{ route('home') }}">Home</a>
+                    <li class="nav-item px-3">
+                        <a class="nav-link navbar-link mx-2 {{ Request::routeIs('home') ? 'active-link' : '' }}" href="{{ route('home') }}">Home</a>
                     </li>
-                    <li class="nav-item">
-                        <a class="nav-link mx-2" href="{{ route('games.index') }}">Series</a>
+                    <li class="nav-item px-3">
+                        <a class="nav-link navbar-link mx-2 {{ Request::routeIs('games.index') ? 'active-link' : '' }}" href="{{ route('games.index') }}">Series</a>
                     </li>
-                    <li class="nav-item">
-                        <a class="nav-link mx-2" href="{{ route('players.rankings') }}">Rankings</a>
+                    <li class="nav-item px-3">
+                        <a class="nav-link navbar-link mx-2 {{ Request::routeIs('players.rankings') ? 'active-link' : '' }}" href="{{ route('players.rankings') }}">Rankings</a>
                     </li>
-                    <li class="nav-item">
-                        <a class="nav-link mx-2" href="{{ route('minigame.index') }}">Who's the Player?</a>
+                    <li class="nav-item px-3">
+                        <a class="nav-link navbar-link mx-2 {{ Request::routeIs('minigame.index') ? 'active-link' : '' }}" href="{{ route('minigame.index') }}">Esportle</a>
                     </li>
-                    <li class="nav-item">
-                        <a class="nav-link mx-2" href="{{ route('calendar') }}">Calendar</a>
+                    <li class="nav-item px-3">
+                        <a class="nav-link navbar-link mx-2 {{ Request::routeIs('calendar') ? 'active-link' : '' }}" href="{{ route('calendar') }}">Calendar</a>
                     </li>
-                    <li class="nav-item">
-                        <a class="nav-link mx-2" href="{{ route('teams.index') }}">Teams</a>
+                    <li class="nav-item px-3">
+                        <a class="nav-link navbar-link mx-2 {{ Request::routeIs('teams.index') ? 'active-link' : '' }}" href="{{ route('teams.index') }}">Teams</a>
                     </li>
-                    <li class="nav-item">
-                        <a class="nav-link mx-2" href="{{ route('transfers.index') }}">Transfers</a>
+                    <li class="nav-item px-3">
+                        <a class="nav-link navbar-link mx-2 {{ Request::routeIs('transfers.index') ? 'active-link' : '' }}" href="{{ route('transfers.index') }}">Transfers</a>
                     </li>
                 </ul>
 
                 <!--Login-->
                 @if (auth()->check())
-                <div class="d-flex justify-content-lg-end align-items-center gap-3 flex-column flex-lg-row">
-                    <div class="dropdown">
-                        <a class="nav-link dropdown-toggle d-flex align-items-center" href="#" id="navbarDropdown" role="button"
-                        data-bs-toggle="dropdown" aria-expanded="false">
-                        <span style="margin-top: 5px;">{{ auth()->user()->name }}</span>
-                        </a>
-                        <ul class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
-                            <li><a class="dropdown-item" href="{{ route('profile.index', auth()->user()) }}">Profile</a></li>
-                            @if (auth()->user()->admin)
-                            <li>
-                                <a class="dropdown-item" href="{{ route('admin.players.index') }}">Admin
-                                panel</a>
-                            </li>
-                            @endif
-                            <li>
-                                <form method="POST" action="{{ route('logout') }}">
-                                    @csrf
-                                    <button class="dropdown-item" type="submit"><a>Log out</a></button>
-                                </form>
-                            </li>
-                        </ul>
+                    <div class="d-flex justify-content-lg-end align-items-center gap-3 flex-column flex-lg-row"
+                        style="margin-right: 50px;">
+                        <div class="dropdown">
+                            <a class="nav-link dropdown-toggle d-flex align-items-center" href="#"
+                                id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                                <span style="margin-top: 5px;">{{ auth()->user()->name }}</span>
+                            </a>
+                            <ul class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
+                                <li><a class="dropdown-item"
+                                        href="{{ route('profile.index', auth()->user()) }}">Profile</a></li>
+                                @if (auth()->user()->admin)
+                                    <li>
+                                        <a class="dropdown-item" href="{{ route('admin.players.index') }}">Admin
+                                            panel</a>
+                                    </li>
+                                @endif
+                                <li>
+                                    <form method="POST" action="{{ route('logout') }}">
+                                        @csrf
+                                        <button class="dropdown-item" type="submit"><a>Log out</a></button>
+                                    </form>
+                                </li>
+                            </ul>
+                        </div>
                     </div>
-                </div>
-                @else
-                <div class="d-flex justify-content-lg-end align-items-center gap-3 flex-column flex-lg-row">
-                    <a href="{{ route('login') }}" class="text-black text-decoration-none">Login</a>
-                    <a href="{{ route('register') }}"
-                    class="text-white text-decoration-none px-3 py-1 bg-primary rounded-4">Register</a>
-                </div>
-                @endif
+                    @else
+                    <div class="d-flex justify-content-lg-end align-items-center gap-3 flex-column flex-lg-row"
+                        style="margin-right: 50px;">
+                        <a href="{{ route('login') }}" class="text-white text-decoration-none login-link">Login</a>
+                        <a href="{{ route('register') }}"
+                            class="text-white text-decoration-none px-3 py-1 bg-red rounded-4 register-link">Register</a>
+                    </div>
+                    @endif
             </div>
         </div>
     </div>
